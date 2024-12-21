018d94244f5274349350161c9cc67774b3b89eaa
Sanjana22-YA <sanjanagowda255@gmail.com>
2024-12-21 12:24:47 +0530

    d3

--- installdocker.sh
+++ installdocker.sh
@@ -9,4 +9,4 @@ docker --version
 sudo yum install git -y
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+docker-compose --version
